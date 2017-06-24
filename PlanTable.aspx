d7b1f0a5b55cf750d948e8ca31b45d6ad493e822
Little-girl-lily <269526462@qq.com>
2017-06-24 22:17:56 +0800

    C# dynamicly produce table
    Use C# ,link to  database,and dynamicly create tables you want.Such as Pic.示例图片

---
+++ PlanTable.aspx
@@ -0,0 +1,111 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="PlanTable.aspx.cs" Inherits="PlanTable" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>安排计划表</title>
+    <link href="LgCss/style.css" rel="stylesheet" type="text/css" />
+    
+    <style type="text/css">
+        div.wrap{width:100%; overflow:hidden; _zoom:1; margin:0 auto;}
+        .wrap ul,li{margin:0; padding:0; list-style:none;}
+        .wrap ul{float:left; display:inline; width:210px;text-align:center;}
+        .wrap ul li{padding:5px; background:#eee; margin:8px; box-shadow:0px 0px 0px #333;
+        border:1px solid #808080;border-radius:10px;height:100px}
+        /*.wrap div{width:100%; font-size:17px;color:#27A9F4;}*/
+        .wrap p{text-align:center;font-size:30px;height:70xp; line-height:70px; vertical-align:middle;font-weight:bold}
+        table,table tr th, table tr td { border:1px solid #BABDBF; }
+        table tr td { width:30px;text-align:center; }
+    </style>
+    <script language="javascript" type="text/javascript">
+    </script>
+    
+</head>
+<body>
+ <div class="place">
+    <span>位置：</span>
+    <ul class="placeul">
+        <li><a href="#">配置页面</a></li>
+    </ul>
+ </div>
+ <form id="form2" runat="server" class="">
+     <div style="float:right;margin:10px 20px 10px 0px;"><%=Year%>年
+         <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+            <asp:ListItem Text="一月" Value="1"></asp:ListItem>
+            <asp:ListItem Text="二月" Value="2"></asp:ListItem>
+            <asp:ListItem Text="三月" Value="3"></asp:ListItem>
+            <asp:ListItem Text="四月" Value="4"></asp:ListItem>
+            <asp:ListItem Text="五月" Value="5"></asp:ListItem>
+            <asp:ListItem Text="六月" Value="6"></asp:ListItem>
+            <asp:ListItem Text="七月" Value="7"></asp:ListItem>
+            <asp:ListItem Text="八月" Value="8"></asp:ListItem>
+            <asp:ListItem Text="九月" Value="9"></asp:ListItem>
+            <asp:ListItem Text="十月" Value="10"></asp:ListItem>
+            <asp:ListItem Text="十一月" Value="11"></asp:ListItem>
+            <asp:ListItem Text="十二月" Value="12"></asp:ListItem>
+        </asp:DropDownList>
+    </div>
+    <div><%=addHtml%></div>
+    <div id="ReplacePoint" runat="server">
+       <table width="100%"> 
+        <tr> 
+           <th>日期</th> 
+             <% for (int i = 0; i < ClassIdArr.Length; i++){%>
+            <th><%=ClassNameArr[i]%> </th> 
+            <% } %>
+        </tr> 
+        <% for (int j = 0; j < dateCount; j++){%>
+        <tr> 
+            <td><%=XMonth%>月<%=(j + 1)%>日</td> 
+            <% for (int k = 0; k < ClassIdArr.Length; k++){%>
+            
+                <td id="<%=ClassIdArr[k]%>"> 
+                <table> 
+                <tr> 
+                <td>00:00~08:00</td> 
+               <%string Temp1 = (j + 1).ToString() + "-" + 1;%>
+               <%string Utemp1 = ClassIdArr[k] + "-" + (j + 1).ToString() + "-" + 1;%>
+               <%if(OrNotCho(j + 1, ClassIdArr[k], "1")){%>
+                    <td><input type='radio' name="<%=Temp1%>" value="<%=Utemp1%>" checked='checked'/></td> 
+               <%}%>
+                <% else{%>
+                
+                    <td><input type='radio' name="<%=Temp1%>" value="<%=Utemp1%>" /></td> 
+                <%}%>
+                </tr> 
+                 <tr> 
+                <td>00:08~08:16</td> 
+                <% string Temp2 = (j + 1).ToString() + "-" + 2;%>
+                <% string Utemp2 = ClassIdArr[k] + "-" + (j + 1).ToString() + "-" + 2;%>
+               <% if (OrNotCho(j + 1, ClassIdArr[k], "2")){%>
+                    <td><input type='radio' name="<%=Temp2%>" value="<%=Utemp2%>" checked='checked'/></td> 
+               <%}%>
+                <% else{%>
+                    <td><input type='radio' name="<%=Temp2%>" value="<%=Utemp2%>" /></td> 
+                <%}%>
+                </tr> 
+                  <tr> 
+                <td>16:00~24:00</td> 
+                <% string Temp3 = (j + 1).ToString() + "-" + 3;%>
+                <% string Utemp3 = ClassIdArr[k] + "-" + (j + 1).ToString() + "-" + 3;%>
+               <%  if (OrNotCho(j + 1, ClassIdArr[k], "3")){%>
+                
+                    <td><input type='radio' name="<%=Temp3%>" value="<%=Utemp3%>" checked='checked'/></td> 
+               <%}%>
+                <% else{%>
+                
+                    <td><input type='radio' name="<%=Temp3%>" value="<%=Utemp3%>" /></td> 
+                <%}%>
+                </tr> 
+                </table> 
+                </td> 
+            <%}%>
+            </tr> 
+        <%}%>
+        </table> 
+        <asp:Button style="margin: 10px auto;width: 10%;float:right;" ID="Button1" runat="server" CssClass="ibtn" Text="确认提交" OnClick="btn_Click"
+                                                UseSubmitBehavior="false" />
+   </div>
+   </form>
+</body>
+</html>
---
+++ PlanTable.aspx.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using PowerDGA.BLL;
+using PowerDGA.Models;
+using System.Collections.Generic;
+using PowerDGA.DAL;
+using System.Data.SqlClient;
+
+public partial class PlanTable : System.Web.UI.Page
+{
+    public int dateCount = 0;
+    public static DateTime newDate = DateTime.Now;//获得当前时间
+    public string[] ClassIdArr;
+    public string[] ClassNameArr;
+    public string addHtml = "";
+    public string BeginHtml = "";
+    public string Year = "";
+    public string Month = "";
+    public string XMonth = "";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // 获取一个月多少天
+        dateCount = newDate.AddDays(1 - newDate.Day).AddMonths(1).AddDays(-1).Day;
+        GetClass();//获取班级的个数
+        Year = newDate.Year.ToString();
+        Month = newDate.Month.ToString();
+        
+        if (!IsPostBack) 
+        {
+            if (this.Session["user"] == null)
+            {
+                Response.Redirect("~/Default.aspx");
+            }
+            else
+            { 
+                //下拉列表选中为当前月
+               this.DropDownList1.Items.FindByValue(Month).Selected = true;
+               XMonth = this.DropDownList1.SelectedValue;
+            }
+        }
+    } 
+    //点击获取表单选中的数据 
+    protected void btn_Click(object sender, EventArgs e)
+    {
+        getPlanValue();
+    }
+    //存储表单选中的数据
+    private void getPlanValue() { 
+       string name="";
+        int count=0;
+        string Day = "";
+        int ClassId = 0; 
+        int q=0;
+        string TimeSlot = "";
+        string[] middleDataArr = new string[93 * ClassIdArr.Length]; 
+        for (int k = 1; k <= dateCount; k++)
+        {
+          for(int t=1;t<=3;t++){
+             name=k+"-"+t;
+             middleDataArr[count] = Request.Form[name];
+             count++;
+          }
+        }
+        //存储之前先把当前月份的记录清除
+        for (int a= 0; a < middleDataArr.Length; a++) {
+           if (middleDataArr[a]!=null)
+             {q++;}
+        } 
+        if(q>0){
+           string sql = "delete from dbo.PlanTable where Month = '" + newDate.Month.ToString() + "'";
+           sql = string.Format(sql, newDate.Month.ToString());
+           DBHelpSQL.ExcuteSQL(sql);
+            }
+
+        for(int m=0;m <middleDataArr.Length;m++){
+          if (middleDataArr[m] != null) {
+           ClassId = int.Parse(middleDataArr[m].Split('-').GetValue(0).ToString());
+           Day = middleDataArr[m].Split('-').GetValue(1).ToString();
+           TimeSlot = middleDataArr[m].Split('-').GetValue(2).ToString();
+           string sql2 = "insert into dbo.PlanTable(ClassId,TimeSlot,Year,Month,Day) values(" + ClassId + "," + TimeSlot + "," + newDate.Year.ToString() + "," + newDate.Month.ToString() + "," + Day + ")";
+           sql2 = string.Format(sql2, ClassId, TimeSlot, newDate.Year.ToString(), newDate.Month.ToString(), Day);
+           DBHelpSQL.ExcuteSQL(sql2);
+            }
+        }
+    }
+    //查询数据库获取班级的个数
+    private void GetClass() 
+    {
+        string sql = "select ClassId,ClassName from dbo.Class";
+        DataSet ForTable = DBHelpSQL.Query(sql);
+        ClassIdArr = new string[ForTable.Tables[0].Rows.Count];
+        ClassNameArr = new string[ForTable.Tables[0].Rows.Count];
+
+        for (int i = 0; i < ForTable.Tables[0].Rows.Count; i++)
+        {
+            ClassIdArr[i] = ForTable.Tables[0].Rows[i]["ClassId"].ToString();
+            ClassNameArr[i] = ForTable.Tables[0].Rows[i]["ClassName"].ToString();
+        }
+    }
+    //判断是否选中
+    public Boolean OrNotCho(int Day,string ClassId,string SlotNo)
+    {
+        string TMonth =this.DropDownList1.SelectedValue;
+       string slotTime = "";
+       string sql = "select TimeSlot from dbo.PlanTable where Year=" + Year + " and Month=" + TMonth + " and Day=" + Day + " and ClassId=" + ClassId;
+       DataSet ForPlanTable = DBHelpSQL.Query(sql);
+       if (ForPlanTable.Tables[0].Rows.Count!=0) {
+           if (ForPlanTable.Tables[0].Rows.Count > 1)
+           {
+               for (int n = 0; n < ForPlanTable.Tables[0].Rows.Count; n++)
+               {
+                   slotTime += ForPlanTable.Tables[0].Rows[n]["TimeSlot"].ToString();
+                   if (n < ForPlanTable.Tables[0].Rows.Count - 1) { slotTime += ","; }
+               }
+           }
+           else { slotTime = ForPlanTable.Tables[0].Rows[0]["TimeSlot"].ToString();}
+       }
+       if(slotTime ==""){
+            return false;
+        }
+        else{
+           int r=slotTime.IndexOf(SlotNo);
+            if (slotTime.IndexOf(SlotNo) >= 0)
+                return true;
+            else return false;
+        }
+      
+    }
+    //下拉选择其他月份时（只能编辑当前月）
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (this.DropDownList1.SelectedValue != newDate.Month.ToString())
+        {
+            this.ReplacePoint.Style.Value = "display:none";
+            //只是查看，用*表示已选中
+            addHtml += "<table width='100%'>";
+            addHtml += "<tr>";
+            addHtml += "   <th>序号</th>";
+            for (int i = 0; i < ClassIdArr.Length; i++)
+            {
+                addHtml += "<th>" + ClassNameArr[i] + "</th>";
+            }
+            addHtml += "</tr>";
+            for (int j = 0; j < dateCount; j++)
+            {
+                addHtml += "<tr>";
+                addHtml += "<td>" + (j + 1) + "</td>";
+                for (int k = 0; k < ClassIdArr.Length; k++)
+                {
+                    addHtml += "<td id=" + ClassIdArr[k] + ">";
+                    addHtml += "<table>";
+                    addHtml += "<tr>";
+                    addHtml += "<td>00:00~08:00</td>";
+                    if (OrNotCho(j+1, ClassIdArr[k], "1"))
+                    {
+                        addHtml += "<td>*</td>";
+                    }
+                    else
+                    {
+                        addHtml += "<td></td>";
+                    }
+                    addHtml += "</tr>";
+                    addHtml += "<tr>";
+                    addHtml += "<td>08:00~16:00</td>";
+                    if (OrNotCho(j + 1, ClassIdArr[k], "2"))
+                    {
+                        addHtml += "<td>*</td>";
+                    }
+                    else
+                    {
+                        addHtml += "<td></td>";
+                    }
+                    addHtml += "</tr>";
+                    addHtml += "<tr>";
+                    addHtml += "<td>16:00~24:00</td>";
+                    if (OrNotCho(j + 1, ClassIdArr[k], "3"))
+                    {
+                        addHtml += "<td>*</td>";
+                    }
+                    else
+                    {
+                        addHtml += "<td></td>";
+                    }
+                    addHtml += "</tr>";
+                    addHtml += "</table>";
+                    addHtml += "</td>";
+                }
+                addHtml += "</tr>";
+            }
+            addHtml += "</table>";
+        }
+        else {
+            this.ReplacePoint.Style.Value = "display:block";  
+        }
+    }
+}
